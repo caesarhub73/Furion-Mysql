b367a0296d345066b8f4f242e7d7a68c2338ddd0
Caesar <34920676+caesarhub73@users.noreply.github.com>
2022-12-12 13:26:58 +0800

    测试提交

--- tests/tests.EntityFramework.Core/DbContexts/DefaultDbContext.cs
+++ tests/tests.EntityFramework.Core/DbContexts/DefaultDbContext.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace tests.EntityFramework.Core
 {
-    [AppDbContext("AADConnection", DbProvider.MySql)]
+    [AppDbContext("AADConnection", $"{DbProvider.MySql}@5.7.32")]
     public class DefaultDbContext : AppDbContext<DefaultDbContext>
     {
         public DefaultDbContext(DbContextOptions<DefaultDbContext> options) : base(options)
